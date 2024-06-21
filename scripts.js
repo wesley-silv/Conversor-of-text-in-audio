6c0ca0ee1deee34c903a05c0553f193bb52ecf78
Wesley Silva <wesleysilvaconceicao@outlook.com>
2024-06-21 18:31:32 -0300

    End the project, all functionalites are attend correctly.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const selectionVoice = document.querySelector('#selection-voice')
-const inputText = document.querySelector('#input-select')
+const inputText = document.querySelector('#input-text')
 const listenAudio = document.querySelector('#listen-btn')
 const downloadText = document.querySelector('#download-text-btn')
 const uploadFile = document.querySelector('#upload-file')
@@ -20,11 +20,48 @@ const updateValues = () => {
   availableVoices.forEach((voz, index) => {
     const option = document.createElement('option')
     option.value = index
-    option.textContent = voz.name 
+    option.textContent = voz.name
     selectionVoice.appendChild(option)
   })
 }
 
-window.speechSynthesis.onvoiceschanged = updateValues;
+window.speechSynthesis.onvoiceschanged = updateValues
 
 // Execute the text such as voice
+selectionVoice.addEventListener('change', () => {
+  voices.voice = availableVoices[selectionVoice.value]
+})
+
+listenAudio.addEventListener('click', () => {
+  voices.text = inputText.value
+
+  window.speechSynthesis.speak(voices)
+})
+
+// Download of text
+downloadText.addEventListener('click', () => {
+  const text = inputText.value
+  const blob = new Blob([text], { type: 'text/plain' })
+
+  const url = URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.href = url
+  a.download = 'text.txt'
+  a.click()
+
+  URL.revokeObjectURL(url)
+})
+
+// Send content file
+uploadFile.addEventListener('change', event => {
+  const file = event.target.files[0]
+
+  if (file) {
+    const reader = new FileReader()
+
+    reader.onload = e => {
+      inputText.value = e.target.result
+    }
+    reader.readAsText(file)
+  }
+})
